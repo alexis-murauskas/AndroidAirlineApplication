9c600779daf94e6de7f22fecbbb56ed0c6c33418
alexis-murauskas <alexis.murauskas@gmail.com>
2020-03-15 21:06:22 -0700

    Init most nav components

--- app/src/main/java/edu/pdx/cs410j/alm9/airline/SearchFragment.java
+++ app/src/main/java/edu/pdx/cs410j/alm9/airline/SearchFragment.java
@@ -4,15 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-import edu.pdx.cs410j.alm9.airline.R;
-
-public class HomeFragment extends Fragment {
+public class SearchFragment extends Fragment {
 
     @Override
     public View onCreateView(
@@ -20,17 +17,17 @@ public class HomeFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_search, container, false);
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_search).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.button_back).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(HomeFragment.this)
-                        .navigate(R.id.action_HomeFragment_to_SearchFragment);
+                NavHostFragment.findNavController(SearchFragment.this)
+                        .navigate(R.id.action_SearchFragment_to_HomeFragment);
             }
         });
     }
