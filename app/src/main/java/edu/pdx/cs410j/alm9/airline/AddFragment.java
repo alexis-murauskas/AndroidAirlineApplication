2fa2a5b50a0ba8bbae5cac6d2cdd69b2cd922c51
alexis-murauskas <alexis.murauskas@gmail.com>
2020-03-16 15:42:23 -0700

    Hide keyboard when exiting add fragment page

--- app/src/main/java/edu/pdx/cs410j/alm9/airline/AddFragment.java
+++ app/src/main/java/edu/pdx/cs410j/alm9/airline/AddFragment.java
@@ -1,9 +1,11 @@
 package edu.pdx.cs410j.alm9.airline;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -22,8 +24,12 @@ public class AddFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_back).setOnClickListener(v ->
+        view.findViewById(R.id.button_back).setOnClickListener(v -> {
+            InputMethodManager inputManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputManager.hideSoftInputFromWindow(v.getWindowToken(), 0);
+
                 NavHostFragment.findNavController(AddFragment.this)
-                        .navigate(R.id.action_AddFragment_to_HomeFragment));
+                        .navigate(R.id.action_AddFragment_to_HomeFragment);
+        });
     }
 }
